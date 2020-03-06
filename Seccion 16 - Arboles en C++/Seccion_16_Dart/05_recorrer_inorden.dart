12733b3eff9bb3b32b589e369f577fbe9a39f13b
diegogarromolina <diego.garromolina@ucr.ac.cr>
2020-03-05 23:32:59 -0600

    In Orden y Pre Orden Arboles en Dart y Python

---
+++ Seccion 16 - Arboles en C++/Seccion_16_Dart/05_recorrer_inorden.dart
@@ -0,0 +1,152 @@
+
+import 'dart:io';
+
+class Nodo {
+
+  int dato = null;
+  Nodo izq = null;
+  Nodo der = null;
+
+  Nodo();
+}
+
+Nodo arbol = new Nodo();
+
+// Función para crear un nuevo nodo
+Nodo crearNodo(int n) {
+  Nodo nuevo_nodo = new Nodo();
+
+  nuevo_nodo.dato = n;
+  nuevo_nodo.izq = new Nodo();
+  nuevo_nodo.der = new Nodo();
+
+  return nuevo_nodo;
+}
+
+// Función para insertar un nuevo nodo
+Nodo insertarNodo(Nodo arbol, int n) {
+  if (arbol.dato == null) { // Si el árbol está vacío
+    arbol = crearNodo(n); 
+  } else { // Si el árbol tiene un nodo o más
+    int valorRaiz = arbol.dato; // Obtener el valor de la raiz
+
+    if (n < valorRaiz) {
+      arbol.izq = insertarNodo(arbol.izq, n); // Si el elemento es menor que la raiz
+    } else {
+      arbol.der = insertarNodo(arbol.der, n); // Si el elemento es mayor que la raiz
+    }
+  }
+
+  return arbol;
+}
+
+// Función para mostrar el arbol completo
+String mostrarArbol(Nodo arbol, int cont) {
+  if (arbol.dato == null) { // Si el árbol está vacío
+    return "";
+  } else { // Si el árbol no está vacío
+    mostrarArbol(arbol.der, cont+1);
+
+    for (int i = 0; i < cont; i++) {
+      stdout.write("   ");
+    }
+
+    print(arbol.dato);
+    mostrarArbol(arbol.izq, cont+1);
+  }
+}
+
+// Función para buscar un elemento en el árbol
+bool buscarNodo(Nodo arbol, int n) {
+  if (arbol.dato == null) {
+    return false;
+  } else if (arbol.dato == n) {
+    return true;
+  } else if (n < arbol.dato) {
+    return buscarNodo(arbol.izq, n);
+  } else {
+    return buscarNodo(arbol.der, n);
+  }
+}
+
+// Función para recorrer el árbol en PreOrden
+String preOrden(Nodo arbol) {
+  if (arbol.dato == null) {
+    return "";
+  } else {
+    stdout.write("${arbol.dato} - ");
+    preOrden(arbol.izq);
+    preOrden(arbol.der);
+  }
+}
+
+// Función para recorrer el árbol en InOrden
+String inOrden(Nodo arbol) {
+  if (arbol.dato == null) {
+    return "";
+  } else {
+    preOrden(arbol.izq);
+    stdout.write("${arbol.dato} - ");
+    preOrden(arbol.der);
+  }
+}
+
+// Función del menú
+void menu() {
+  int dato, opcion, contador = 0;
+
+  do {
+    stdout.writeln("\t.:MENU:.");
+    stdout.writeln("1. Insertar un nuevo nodo.");
+    stdout.writeln("2. Mostrar el árbol completo.");
+    stdout.writeln("3. Buscar un elemento en el árbol.");
+    stdout.writeln("4. Recorrer el árbol en PreOrden.");
+    stdout.writeln("5. Recorrer el árbol en InOrden.");
+    stdout.writeln("6. Salir.");
+    stdout.write("Opción: ");
+    opcion = int.parse(stdin.readLineSync());
+
+    switch (opcion) {
+      case 1:
+        stdout.write("\nDigite un número: ");
+        dato = int.parse(stdin.readLineSync());
+        arbol = insertarNodo(arbol, dato);
+        print("");
+        break;
+      case 2:
+        print("\nMostrando el árbol completo:");
+        String _a = mostrarArbol(arbol, contador);
+        print("");
+        break;
+      case 3:
+        stdout.write("\nDigite el elemento a buscar: ");
+        dato = int.parse(stdin.readLineSync());
+        if (buscarNodo(arbol, dato)) {
+          print("El elemento $dato ha sido encontrado en el árbol.");
+        } else {
+          print("El elemento $dato no ha sido encontrado en el árbol.");
+        }
+        break;
+      case 4:
+        print("\nRecorrido en PreOrden.");
+        String _a = preOrden(arbol);
+        print("");
+        break;
+      case 5:
+        print("\nRecorrido en InOrden.");
+        String _a = inOrden(arbol);
+        print("");
+        break;
+      
+      default:
+        break;
+    }
+  } while (opcion != 6);
+}
+
+// Función principal main()
+void main() {
+
+  menu();
+  
+}
---
+++ Seccion 16 - Arboles en C++/Seccion_16_Python/05_recorrer_inorden.py
@@ -0,0 +1,119 @@
+
+class Nodo:
+    
+    def __init__(self):
+        self.izq = None
+        self.der = None
+        self.dato = None
+        
+# Función para crear un nuevo nodo
+def crear_nodo(n: int):
+    nuevo_nodo = Nodo()
+    
+    nuevo_nodo.dato = n
+    nuevo_nodo.izq = Nodo()
+    nuevo_nodo.der = Nodo()
+    
+    return nuevo_nodo
+
+# Función para insertar elementos al arbol
+def insertar_nodo(arbol: Nodo, n: int):
+    if arbol.dato == None: # Si el árbol está vacío
+        arbol = crear_nodo(n)
+    else: # Si el árbol tiene un nodo o más
+        valor_raiz = arbol.dato # Obtenemos el valor raiz
+        
+        if n < valor_raiz:
+            arbol.izq = insertar_nodo(arbol.izq, n) # Si el elemento es menor que la raiz
+        else:
+            arbol.der = insertar_nodo(arbol.der, n) # Si el elemento es mayor que la raiz
+    return arbol
+
+def mostrar_arbol(arbol: Nodo, cont: int):
+    if arbol.dato == None: # Si el arbol está vacío
+        return ''
+    else: # Si el árbol no está vacío
+        mostrar_arbol(arbol.der, cont+1)
+        
+        for i in range(cont):
+            print("   ", end='')
+        
+        print(arbol.dato)
+        mostrar_arbol(arbol.izq, cont+1)
+
+# Función para buscar un elemento en el árbol
+def buscar_nodo(arbol: Nodo, n: int):
+    if arbol.dato == None:
+        return False
+    elif arbol.dato == n:
+        return True
+    elif n < arbol.dato:
+        return buscar_nodo(arbol.izq, n)
+    else:
+        return buscar_nodo(arbol.der, n)
+
+# Función para recorrer el árbol en PreOrden
+def pre_orden(arbol: Nodo):
+    if arbol.dato == None:
+        return ""
+    else:
+        print("{} - ".format(arbol.dato), end="")
+        pre_orden(arbol.izq)
+        pre_orden(arbol.der)
+
+# Función para recorrer el árbol en InOrden
+def in_orden(arbol: Nodo):
+    if arbol.dato == None:
+        return ""
+    else:
+        pre_orden(arbol.izq)
+        print("{} - ".format(arbol.dato), end="")
+        pre_orden(arbol.der)
+
+def menu():
+    dato: int
+    opcion: int = 0
+    contador: int = 0
+    
+    arbol = Nodo()
+    
+    while opcion != 6:
+        print("\t.:MENU:.")
+        print("1. Insertar un nuevo nodo.")
+        print("2. Mostrar el arbol.")
+        print("3. Buscar un elemento en el árbol.")
+        print("4. Recorrer el árbol en PreOrden.")
+        print("5. Recorrer el árbol en InOrden.")
+        print("6. Salir.")
+        opcion = int(input("Opción: "))
+        
+        if opcion == 1:
+            dato = int(input("\nDigite un número: "))
+            arbol = insertar_nodo(arbol, dato)
+        elif opcion == 2:
+            print("\nMostrando el árbol completo:\n")
+            mostrar_arbol(arbol, contador)
+            print()
+        elif opcion == 3:
+            dato = int(input("\nDigite el elemento a buscar: "))
+            if buscar_nodo(arbol, dato):
+                print("El elemento {} ha sido encontrado en el árbol.".format(dato))
+            else:
+                print("El elemento {} no se ha encontrado en el árbol.".format(dato))
+        elif opcion == 4:
+            print("\nRecorrido en PreOrden:")
+            pre_orden(arbol)
+            print()
+        elif opcion == 5:
+            print("\nRecorrido en InOrden:")
+            in_orden(arbol)
+            print()
+
+def main():
+    
+    menu()
+
+if __name__ == "__main__":
+    main()
+    
+    
